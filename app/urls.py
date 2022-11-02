5664ab49ed69506fd8aa3da1ea2ddd5cc6b59846
Aby Sebastian <abysebastian98@gmail.com>
2022-11-02 22:47:51 +0530

    learned diff b/w APIView and ModelViewSet

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework.urlpatterns import format_suffix_patterns
 from .views import *
 
 # define the router
@@ -10,5 +11,8 @@ router.register(r'^Book', BookViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('library/', BookAPIView.as_view()),
     path('api-auth/', include('rest_framework.urls'))    
 ]
+
+# urlpatterns = format_suffix_patterns(urlpatterns)
--- app/views.py
+++ app/views.py
@@ -1,12 +1,33 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from .serializers import BookSerializer
 from .models import Book
 
 # Create your views here.
 
 # create a viewset
+# modelviewset creates view according to models and would have all 
+# required get, post, etc.. methods with minimal code to write
 class BookViewSet(viewsets.ModelViewSet):
 	queryset = Book.objects.all()
 	# specify serializer to be used
 	serializer_class = BookSerializer
+
+class BookAPIView(APIView):
+	"""
+	List all Books or Create a new Book
+	"""
+	def get(self, request, format=None):
+		books = Book.objects.all()	
+		serializer = BookSerializer(books, many=True)
+		return Response(serializer.data)
+
+	def post(self, request, format=None):
+		serializer = BookSerializer(data=request.data)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data, status=status.HTTP_201_CREATED)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
