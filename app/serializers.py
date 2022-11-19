2b2d9832fb2818f1e392ba7adf1028073d638610
Aby Sebastian <abysebastian98@gmail.com>
2022-11-19 23:57:04 +0530

    created new models to add multiple images

--- app/serializers.py
+++ app/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from .models import Book
+from .models import Book, BookImages
+
+
+class BookImageSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = BookImages
+		fields = ('id', 'book', 'image')
 
 class BookSerializer(serializers.ModelSerializer):
+	image = BookImageSerializer(many=True)
 	class Meta:
 		model = Book
-		fields = ('id', 'title', 'description')
+		fields = ['id', 'title', 'description', 'image']
